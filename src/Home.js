647036e263332ae1e02eb7d854810d62a401c5aa
Lambda_School_Loaner_198 <lambda_school_loaner_198@Lambda-School-Loaner-198s-MacBook-Pro.local>
2019-12-31 09:05:48 -0800

    fixed some typos

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
+// import earth from "./earth.png";
+import "./App.css";
 
 export default function Home () {
   
         return (
           <div>
-            <h2>NASA API</h2>
+            <h2>NASA API </h2>
+            <div className="group">
             <img src="https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg" alt="nasa logo"/>
+            {/* <img className ="earth" src={earth} alt="world" /> */}
+            </div>
            <br></br>
            
-           <h3>Click the Links above to see different API's in use</h3>
+           <h3>Click the LINKS above to see different APIs in use</h3>
            <h3>This is also a single page set up</h3>
           
           </div>
