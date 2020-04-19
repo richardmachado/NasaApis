db6785d2bebe9664404b872f2492260229dc7ecc
Richard Machado <richardmachado@MacBook-Pro.local>
2020-04-19 09:57:37 -0700

    changed NEO page to new format

--- src/API/NEO.js
+++ src/API/NEO.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../App.css";
-import loading from '../loading.jpg'
-import {Card } from 'react-bootstrap';
+
+import classNames from 'classnames';
+
+import NEOKey from "./NEOKey"
+import "./NEO.css"
+
+
 
 
 // function for today so that it automatically updates the site
@@ -19,7 +23,7 @@ today = yyyy + '-' + mm + '-' + dd ;
 // end of today function
 
 
-function NEO() {
+function NEO(props) {
   const [neo, setNeo] = useState([]);
 
 
@@ -28,7 +32,7 @@ function NEO() {
 
       .get(`https://api.nasa.gov/neo/rest/v1/feed/?date=${today}?detailed=true&api_key=X7831OHO7jNbCUFp6ZquUbFjI2txHRDvsbay1fU4`)   
       .then(response => {
-   
+          
           setNeo(response.data.near_earth_objects[today]);
    
       })
@@ -37,30 +41,38 @@ function NEO() {
       });
   }, []);
   if (!neo) {
-    return <div> <img src={loading} alt= "loading"></img> </div>;
+    return <div>Loading...</div>;
   }
   return (
-    <div className="App">
-        <h1> Near Earth Objects  for {today}</h1> 
-        <p className="paragraph">The challenge on this API was NASA uses GMT time while Date.now uses the time on your computer. The information would not display after 5PM Pacific since it was already the following day according to GMT</p>
-        {neo.map(rock =>{
-           return (
-          
-  <Card key = {rock.name} id="Card" border="light" text="light"style={{ width: '30rem' }}>
-    <Card.Header  >Object Name: {rock.name}</Card.Header>
-    <Card.Body>
-      <Card.Title >Size : {rock.estimated_diameter.miles.estimated_diameter_max} miles</Card.Title>
-      <Card.Text >
-      Potentially hazardous: {`${rock.is_potentially_hazardous_asteroid}`}
-      </Card.Text>
-    </Card.Body>
-  </Card>
-          
-            )
-        })}
-
- 
+    <div className="body">
+      <div className="container">
+        <h1 className="display-4 my3"><span classname="text-dark"> Near Earth Objects </span>  for  {today}</h1>        
+          <NEOKey />  
+        {neo.map(rock => {
+            return (
+            <ul className="list-group">
+      <h4 className="mb3" key={rock.name} style={{width:'500px'}}>Object Name: <span className={classNames({
+              'text-danger': !rock.name,
+              'text-success': rock.name})}>{rock.name}</span></h4>    
+          <ul className="group-list">      
+        <li className="group-list-item" >Size : {rock.estimated_diameter.miles.estimated_diameter_max} miles</li>
+        {/* <li className="group-list-item">
+        Potentially hazardous: <span className={classNames({
+              'text-danger': rock.is_potentially_hazardous_asteroid,
+              'text-success': !rock.is_potentially_hazardous_asteroid})}>
+                      {`${rock.is_potentially_hazardous_asteroid ? 'Yes' : 'No'}`}
+                      </span>          
+                    </li> */}
+                    </ul>                  
+                    </ul>
+  )
+  })}
+      </div>
     </div>
+
+
+    
+    
   );
 }
 
---
+++ src/API/NEOKey.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NEOKey() {
+    return (
+        <div className="my-3">
+            <p>
+                <span className="px-3 mr-2 bg-success" /> = Safe Asteroid
+            </p>
+          
+            <p>
+                <span className="px-3 mr-2 bg-danger" /> = Dangerous Asteroid
+            </p>
+        </div>
+    )
+}
