09b55827398029df7741724b37ea87f4ae843833
Richard Machado <richardmachado@MacBook-Pro.local>
2020-04-25 08:24:13 -0700

    fixing routing issue with redirects

--- src/Home.js
+++ src/Home.js
@@ -5,20 +5,20 @@ import "./App.css";
 export default function Home () {
   
         return (
-          <div>
+          <>
             <div className="homepage">
             <h2>NASA APIs </h2>
             <div className="group">
             <img src="https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg" alt="nasa logo"/>
           
             </div>
-           <br></br>
+           <hr />
            
            <h3>Click the links above to see different APIs in use</h3>
            <h3>This is also a single page set up</h3>
            <p>This was a valuable exercise in consuming different API's that return different json file types</p>
           
             </div>
-            </div>
+            </>
         );
       }
