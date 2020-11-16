1537c9665a50e28911c99a6671bc56ec71efa61e
Richard Machado <mmachado.richard@gmail.com>
2020-11-16 10:10:19 -0800

    fix backgrounds so only homepage has launch

--- src/API/NEOKey.js
+++ src/API/NEOKey.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default function NEOKey() {
     return (
         <div style={{backgroundColor:"black"}} className="my-3">
-            <p style={{fontSize:'3rem'}}>
-                <span className="px-5 mr-2 bg-success" /> = Safe Asteroid
+            <p style={{fontSize:'2rem'}}>
+                <span className="px-4 mr-2 bg-success" /> = Safe Asteroid
             </p>
-            <p style={{fontSize:'3rem'}}>
-                <span className="px-5 mr-2 bg-danger" /> = Potentially Dangerous Asteroid
+            <p style={{fontSize:'2rem'}}>
+                <span className="px-4 mr-2 bg-danger" /> = Potentially Dangerous Asteroid
             </p>
         </div>
     )
