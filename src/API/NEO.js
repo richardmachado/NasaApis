f35ed149dc41688e086a1beeb34106d11f1b1230
Richard Machado <mmachado.richard@gmail.com>
2020-12-01 10:47:53 -0800

    add date picker to NEO

--- src/API/NEO.js
+++ src/API/NEO.js
@@ -7,21 +7,31 @@ import classNames from 'classnames';
 import NEOKey from "./NEOKey"
 import "./NEO.css"
 
-// function for today so that it automatically updates the site for the axios call
-
-// var today = new Date()
-// console.log(today)
-// var dd = String(today.getUTCDate()).padStart(2, '0');
-// var mm = String(today.getMonth()+1).padStart(2, '0'); //January is 0!
-// var yyyy = today.getFullYear();
-// today = yyyy + '-' + mm + '-' + dd;
 
 var today = moment().utc(true).format("YYYY-MM-DD");
+// var todayheader = moment().utc(true).format("MM-DD-YYYY");
 
+var tomorrow = moment().utc(true).add(1, 'd').format("YYYY-MM-DD")
+console.log(tomorrow)
 
-//today for header
-// const today2 = mm + '-' + dd + '-' + yyyy;
-const today2 = "Today"
+var dayaftertomorrow = moment().utc(true).add(2, 'd').format("YYYY-MM-DD")
+console.log(dayaftertomorrow)
+
+var twodaysaftertomorrow = moment().utc(true).add(3, 'd').format("YYYY-MM-DD")
+console.log(twodaysaftertomorrow)
+
+var threedaysaftertomorrow = moment().utc(true).add(4, 'd').format("YYYY-MM-DD")
+console.log( threedaysaftertomorrow)
+
+var fourdaysaftertomorrow = moment().utc(true).add(5, 'd').format("YYYY-MM-DD")
+console.log(fourdaysaftertomorrow)
+
+var fivedaysaftertomorrow = moment().utc(true).add(6, 'd').format("YYYY-MM-DD")
+console.log(fivedaysaftertomorrow )
+
+var sixdaysaftertomorrow = moment().utc(true).add(7, 'd').format("YYYY-MM-DD")
+console.log(sixdaysaftertomorrow)
+
 
 
 // function to add commas to really large numbers
@@ -35,24 +45,47 @@ const KEY = process.env.REACT_APP_KEY;
 
 function NEO() {
   const [neo, setNeo] = useState([]);
+  const [date, setDate] = useState(today)
+
+  const handleSubmit = e => {
+    setDate(e.target.value);
+
+  }
 
   useEffect(() => {
     axios
       .get(`https://api.nasa.gov/neo/rest/v1/feed/?date=${today}?detailed=true&api_key=${KEY}`)   
       .then(response => {
-        // console.log(response.data.near_earth_objects[today])
-        setNeo(response.data.near_earth_objects[today])
+        console.log(response.data.near_earth_objects)
+        setNeo(response.data.near_earth_objects[date])
       })
       .catch(err => {
         console.log(err);
       });
-  },[]);
+  },[date]);
 
   return (
     <div className="body">
       <div className="container">
-        <h1 className="display-4 my3"><span className="text-dark"> Near Earth Objects </span>  for  {today}</h1>        
-          <NEOKey />  
+        <h1 className="display-4 my3"><span className="text-dark"> Near Earth Objects </span>  for  {date}</h1>        
+        <NEOKey />  
+
+        <label className="prompt" htmlFor="date">
+         Select a Date
+         <select name="date" 
+          onChange={e => handleSubmit(e)}
+          form="date">
+            <option value={today}>{today}</option>  
+            <option value={tomorrow}>{tomorrow}</option>
+            <option value={dayaftertomorrow}>{dayaftertomorrow}</option>
+            <option value={twodaysaftertomorrow}>{twodaysaftertomorrow}</option> 
+            <option value={threedaysaftertomorrow}>{threedaysaftertomorrow}</option> 
+            <option value={fourdaysaftertomorrow}>{fourdaysaftertomorrow}</option> 
+            <option value={fivedaysaftertomorrow}>{fivedaysaftertomorrow}</option> 
+            <option value={sixdaysaftertomorrow}>{sixdaysaftertomorrow}</option> 
+        </select>
+      </label>
+
         {neo.map(rock => {
           return (
            <div key={rock.id}>
