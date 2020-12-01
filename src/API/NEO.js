524be0fb991905f624200ffee6264db824dc1f3c
Richard Machado <mmachado.richard@gmail.com>
2020-11-30 17:50:52 -0800

    not sure why the month didn't change to december, stuck at november

--- src/API/NEO.js
+++ src/API/NEO.js
@@ -10,15 +10,12 @@ import "./NEO.css"
 
 var today = new Date()
 var dd = String(today.getUTCDate()).padStart(2, '0');
-var mm = String(today.getMonth()+1).padStart(2, '0'); //January is 0!
+var mm = String(today.getMonth()+2).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
-today = yyyy + '-' + mm + '-' + dd ;
+today = yyyy + '-' + mm + '-' + dd;
+console.log(today)
 
 // end of today function
-
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
 
 const KEY = process.env.REACT_APP_KEY;
 
@@ -29,7 +26,9 @@ function NEO() {
     axios
       .get(`https://api.nasa.gov/neo/rest/v1/feed/?date=${today}?detailed=true&api_key=${KEY}`)   
       .then(response => {
+        console.log(response.data.near_earth_objects[today])
         setNeo(response.data.near_earth_objects[today])
+
       })
       .catch(err => {
         console.log(err);
@@ -43,25 +42,29 @@ function NEO() {
           <NEOKey />  
         {neo.map(rock => {
           return (
-           <div className="results" key={rock.id}>
-            <h4 className="mb3">Object Name: <span className={classNames({
+           <div key={rock.id}>
+            <h4 className="mb3" key={rock.name} >Object Name: <span className={classNames({
               'text-danger': !rock.name,
               'text-success': rock.name
-            })}>{rock.name}</span></h4>
+            })}>{rock.name}</span></h4> 
+             
               <p className="size">
                 Size(Maximum Diameter): {Math.round(((rock.estimated_diameter.feet.estimated_diameter_max)) * 100) / 100} feet
-              </p>
-              <p className="size">
+ 
+              </p> 
+               <p className="size">
                 Closest Approach Date: {rock.close_approach_data[0].close_approach_date}
                 </p>
                 <p className="size">
-                  Miss Distance : {numberWithCommas(Math.round(rock.close_approach_data[0].miss_distance.miles))}
+                  Miss Distance : {Math.round(((rock.close_approach_data[0].miss_distance.miles)) * 100) / 100}
                     <span> </span>miles
                 </p>
                 <p className="size">
-                Relative Velocity : {numberWithCommas(Math.round(rock.close_approach_data[0].relative_velocity.miles_per_hour))}
+                Relative Velocity : {Math.round(((rock.close_approach_data[0].relative_velocity.miles_per_hour)) * 100) / 100}
                     <span> </span>miles per hour
                 </p>
+
+
             </div>                   
   )
   })}
