1f15f231bd6512e93f033c9230c11b0f2d27cc6e
Richard Machado <mmachado.richard@gmail.com>
2020-12-01 09:50:29 -0800

    switched date on NEO from javascript to moment()

--- src/API/NEO.js
+++ src/API/NEO.js
@@ -1,26 +1,36 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import moment from 'moment';
 
 import classNames from 'classnames';
 
 import NEOKey from "./NEOKey"
 import "./NEO.css"
 
-// function for today so that it automatically updates the site
+// function for today so that it automatically updates the site for the axios call
 
-var today = new Date()
-var dd = String(today.getUTCDate()).padStart(2, '0');
-var mm = String(today.getMonth()+1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
-today = yyyy + '-' + mm + '-' + dd;
+// var today = new Date()
+// console.log(today)
+// var dd = String(today.getUTCDate()).padStart(2, '0');
+// var mm = String(today.getMonth()+1).padStart(2, '0'); //January is 0!
+// var yyyy = today.getFullYear();
+// today = yyyy + '-' + mm + '-' + dd;
+
+var today = moment().utc(true).format("YYYY-MM-DD");
 
 
-// end of today function
+//today for header
+// const today2 = mm + '-' + dd + '-' + yyyy;
+const today2 = "Today"
+
+
+// function to add commas to really large numbers
 
 function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+// secret NASA Key
 const KEY = process.env.REACT_APP_KEY;
 
 function NEO() {
@@ -32,7 +42,6 @@ function NEO() {
       .then(response => {
         // console.log(response.data.near_earth_objects[today])
         setNeo(response.data.near_earth_objects[today])
-
       })
       .catch(err => {
         console.log(err);
@@ -51,7 +60,6 @@ function NEO() {
               'text-danger': !rock.name,
               'text-success': rock.name
             })}>{rock.name}</span></h4> 
-             
               <p className="size">
               Size(Maximum Diameter): {Math.round(((rock.estimated_diameter.feet.estimated_diameter_max)) * 100) / 100} feet
               </p> 
@@ -66,8 +74,6 @@ function NEO() {
                 Relative Velocity : {numberWithCommas(Math.round(rock.close_approach_data[0].relative_velocity.miles_per_hour))}
                     <span> </span>miles per hour
                 </p>
-
-
             </div>                   
   )
   })}
