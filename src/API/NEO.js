c1b3d46ed8ec81a863bbce713f5cff03768a827f
Richard Machado <mmachado.richard@gmail.com>
2020-12-02 09:10:14 -0800

    going to to javascript date

--- src/API/NEO.js
+++ src/API/NEO.js
@@ -96,62 +96,31 @@
 // export default NEO;
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import moment from 'moment';
+
 import classNames from 'classnames';
+
 import NEOKey from "./NEOKey"
 import "./NEO.css"
 
-// function for today so that it automatically updates the site for the axios call
+// function for today so that it automatically updates the site
 
-// var today = new Date()
-// console.log(today)
-// var dd = String(today.getUTCDate()).padStart(2, '0');
-// var mm = String(today.getMonth()+1).padStart(2, '0'); //January is 0!
-// var yyyy = today.getFullYear();
-// today = yyyy + '-' + mm + '-' + dd;
-
-var today = moment().utc(true).format("YYYY-MM-DD");
-// var todayheader = moment().utc(true).format("MM-DD-YYYY");
-
-var tomorrow = moment().utc(true).add(1, 'd').format("YYYY-MM-DD")
-console.log(tomorrow)
-
-var dayaftertomorrow = moment().utc(true).add(2, 'd').format("YYYY-MM-DD")
-console.log(dayaftertomorrow)
-
-var twodaysaftertomorrow = moment().utc(true).add(3, 'd').format("YYYY-MM-DD")
-console.log(twodaysaftertomorrow)
-
-var threedaysaftertomorrow = moment().utc(true).add(4, 'd').format("YYYY-MM-DD")
-console.log( threedaysaftertomorrow)
-
-var fourdaysaftertomorrow = moment().utc(true).add(5, 'd').format("YYYY-MM-DD")
-console.log(fourdaysaftertomorrow)
-
-var fivedaysaftertomorrow = moment().utc(true).add(6, 'd').format("YYYY-MM-DD")
-console.log(fivedaysaftertomorrow )
-
-var sixdaysaftertomorrow = moment().utc(true).add(7, 'd').format("YYYY-MM-DD")
-console.log(sixdaysaftertomorrow)
-
- function numberWithCommas(x) {
-   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
- }
+var today = new Date()
+var dd = String(today.getUTCDate()).padStart(2, '0');
+var mm = String(today.getMonth()+1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+today = yyyy + '-' + mm + '-' + dd;
 
 
+// end of today function
 
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
-// function to add commas to really large numbers
 const KEY = process.env.REACT_APP_KEY;
 
 function NEO() {
   const [neo, setNeo] = useState([]);
-  const [date, setDate] = useState(today)
-
-  const handleSubmit = e => {
-    setDate(e.target.value);
-
-  }
 
   useEffect(() => {
     axios
@@ -159,37 +128,18 @@ function NEO() {
       .then(response => {
         // console.log(response.data.near_earth_objects[today])
         setNeo(response.data.near_earth_objects[today])
-        console.log(response.data.near_earth_objects)
-        setNeo(response.data.near_earth_objects[date])
+
       })
       .catch(err => {
         console.log(err);
       });
-  },[date]);
+  },[]);
 
   return (
     <div className="body">
       <div className="container">
-      
-        <h1 className="display-4 my3"><span className="text-dark"> Near Earth Objects </span>  for  {date}</h1>        
-        <NEOKey />  
-
-        <label className="prompt" htmlFor="date">
-         Select a Date
-         <select name="date" 
-          onChange={e => handleSubmit(e)}
-          form="date">
-            <option value={today}>{today}</option>  
-            <option value={tomorrow}>{tomorrow}</option>
-            <option value={dayaftertomorrow}>{dayaftertomorrow}</option>
-            <option value={twodaysaftertomorrow}>{twodaysaftertomorrow}</option> 
-            <option value={threedaysaftertomorrow}>{threedaysaftertomorrow}</option> 
-            <option value={fourdaysaftertomorrow}>{fourdaysaftertomorrow}</option> 
-            <option value={fivedaysaftertomorrow}>{fivedaysaftertomorrow}</option> 
-            <option value={sixdaysaftertomorrow}>{sixdaysaftertomorrow}</option> 
-        </select>
-      </label>
-
+        <h1 className="display-4 my3"><span className="text-dark"> Near Earth Objects </span>  for  {today}</h1>        
+          <NEOKey />  
         {neo.map(rock => {
           return (
            <div key={rock.id}>
@@ -197,6 +147,7 @@ function NEO() {
               'text-danger': !rock.name,
               'text-success': rock.name
             })}>{rock.name}</span></h4> 
+             
               <p className="size">
               Size(Maximum Diameter): {Math.round(((rock.estimated_diameter.feet.estimated_diameter_max)) * 100) / 100} feet
               </p> 
@@ -211,6 +162,8 @@ function NEO() {
                 Relative Velocity : {numberWithCommas(Math.round(rock.close_approach_data[0].relative_velocity.miles_per_hour))}
                     <span> </span>miles per hour
                 </p>
+
+
             </div>                   
   )
   })}
@@ -218,4 +171,5 @@ function NEO() {
     </div>
   );
 }
+
 export default NEO;
